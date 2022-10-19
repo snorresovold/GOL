f060e133e185c89d40c63c49bb9baf835e1d06f0
snorresovold <snorresovold@gmail.com>
2022-10-19 11:01:26 +0200

    now shows a triangle to the screen

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
     "log"
     "runtime"
-
+    "fmt"
+    "strings"
     "github.com/go-gl/gl/v4.1-core/gl" // OR: github.com/go-gl/gl/v2.1/gl
     "github.com/go-gl/glfw/v3.3/glfw"
 )
@@ -11,11 +12,31 @@ import (
 const ( // constants such as width and height
     width  = 500
     height = 500
+
+    // strings with source code for 2 shaders
+    vertexShaderSource = `
+        #version 410
+        in vec3 vp;
+        void main() {
+            gl_Position = vec4(vp, 1.0);
+        }
+    ` + "\x00"
+
+    fragmentShaderSource = `
+        #version 410
+        out vec4 frag_colour;
+        void main() {
+            frag_colour = vec4(1, 1, 1, 1);
+        }
+    ` + "\x00"
 )
 
 // it's a triangle, duh
 var (
-    triangle = []float32{
+    triangle = []float32{ // always use float32 when providing verices to Opengl
+        // values represented as xyz
+        // ranges from -1 to 1
+        // we're not using 3D so z is always 0
         0, 0.5, 0, // top
         -0.5, -0.5, 0, // left
         0.5, -0.5, 0, // right
@@ -30,8 +51,10 @@ func main() {
 
     program := initOpengl()
 
+    vao := makeVao(triangle)
+    
     for !window.ShouldClose() {
-        draw(window, program) // when the program is closing, draw
+        draw(vao, window, program) // when the program is closing, draw
     }
 }
 
@@ -66,19 +89,81 @@ func initOpengl() uint32 {
     }
     version := gl.GoStr(gl.GetString(gl.VERSION))
     log.Println("Opengl version", version)
+    
+    vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+    if err != nil {
+        panic(err)
+    }
+    fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+    if err != nil {
+        panic(err)
+    }
 
     prog := gl.CreateProgram()
+    gl.AttachShader(prog, vertexShader)
+    gl.AttachShader(prog, fragmentShader)
     gl.LinkProgram(prog)
     return prog
 }
 
-func draw(window *glfw.Window, prog uint32 ) {
+// Here we call **makeVao** to get our **vao** reference from the **triangle** points we defined before, and pass it as a new argument to the **draw** function:
+
+func draw(vao uint32, window *glfw.Window, prog uint32 ) {
     gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT) // removes anything that was drawn last frame
     gl.UseProgram(prog) // using our program refrence
-    
+
+    gl.BindVertexArray(vao)
+    // divides by 3 because there are 3 values so it knows how many vertices to draw
+    gl.DrawArrays(gl.TRIANGLES, 0, int32(len(triangle) / 3))
+
     glfw.PollEvents() // checks for events such as keystrokes and mouseclicks
     
     // swaps the buffers wich is important because opengl actually uses 2 canvases, one visible and one invisible
     // when drawing we draw to the invisible one and then later swap them with the bufferswap method
     window.SwapBuffers()
+}
+
+// makeVao initializes and returns a vertex array from the points provided
+// vao = Vertex Array Object
+// vbo = vertex buffer object
+func makeVao(points []float32) uint32 {
+    var vbo uint32 // create a buffer
+    gl.GenBuffers(1, &vbo)
+    gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+    // generate, bind and giv the buffer data
+
+    // 4 x len(points) because a 32 bit float has 4 bytes
+    gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+
+    var vao uint32 // create vertex array
+    gl.GenVertexArrays(1, &vao)
+    gl.BindVertexArray(vao)
+    gl.EnableVertexAttribArray(0)
+    // generate, bind and give it attributes
+    gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+    gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
+    return vao
+}
+
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+    shader := gl.CreateShader(shaderType)
+
+    csourcers, free := gl.Strs(source)
+    gl.ShaderSource(shader, 1, csourcers, nil)
+    free()
+    gl.CompileShader(shader)
+
+    var status int32
+    gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+    if status == gl.FALSE {
+        var logLength int32
+        gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+        log := strings.Repeat("\x00", int(logLength+1))
+        gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+        return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+    }
+    return shader, nil
 }
